46a6b00aea5dfa17fdd3c4c01fe68267d77f4f70
GrbigO <gorazbozorg@gmail.com>
2025-06-03 18:59:23 +0330

    Add Model as 'Description' for AI

---
+++ leonbot/core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Description(models.Model):
+	description0 = models.TextField(blank=True)
+	description1 = models.TextField(blank=True)
+	description2 = models.TextField(blank=True)
+	description3 = models.TextField(blank=True)
+
