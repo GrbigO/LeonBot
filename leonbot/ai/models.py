8a28efb59da39b12408396ba7e96e5d653407af3
GrbigO <gorazbozorg@gmail.com>
2025-06-04 23:16:34 +0330

    Inheritance of abstract class and new feature

--- leonbot/ai/models.py
+++ leonbot/ai/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 from django.conf import settings
 
+from leonbot.core.models import ModelWithDescriptions, ModelWithDateTime
 
 
-class AI(models.Model):
+class AI(ModelWithDescriptions, ModelWithDateTime):
 	name = models.CharField(max_length=50, primary_key=True, null=False, editable=False, serialize=False)
-	model = models.CharField(max_length=100)
 	key = models.CharField(max_length=120)
-	msgs = models.JSONField()
-
-
-	descriptions = models.OneToOneField(to="core.Description", on_delete=models.CASCADE)
-
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+	msgs = models.JSONField(default=dict)
 
 	created_by = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="created_by")
 	updated_by = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="updated_by")
 
+	class Meta:
+		db_table = "ai_model"
 
+	def get_description(self) -> set[str]:
+		return {
+			self.description0,
+			self.description1,
+			self.description2,
+			self.description3,
+		}
+
+
+def update_msg(user_value, ai_value, old_values):
+	return NotImplemented
