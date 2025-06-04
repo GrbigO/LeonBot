f1dc69f3a725669834939e318cadfa51e60ac400
GrbigO <gorazbozorg@gmail.com>
2025-06-04 17:25:16 +0330

    Fix Bug but not working DSL...

--- leonbot/chat/enums.py
+++ leonbot/chat/enums.py
@@ -1,7 +1,12 @@
+from typing import Optional
+
 from enum import Enum
 
+from ..ai.manage import AIManager
+from ..core.bot.settings import bot_settings
 
-class DSLMethod(Enum):
+
+class LeonDSL(Enum):
 	delete = None
 	add = None
 	update = None
@@ -9,18 +14,47 @@ class DSLMethod(Enum):
 	train = None
 	user = None
 	perm = None
-	role = None
-	conf = None
+	setting = None
 
 	__msg_for = None
-	__bag = None
+	__bug = None
 	__restart = None
 
+	__all__ = None
+	__func__ = dict()
+
 	@classmethod
-	def parse(cls):
-		pass
+	async def _parse_code(cls, code, message) -> tuple[Optional[str], dict]:
+		arg = {}
+		func_name = "_"
+		for name in code:
+			if name.startswith("--"):
+				arg = name
+			else:
+				func_name = func_name + name + "_"
+
+		func = cls.__func__.get(func_name)
+		if func is None:
+			return None, {}
+
+		return await func(message=message, arg=arg)
+
+	@classmethod
+	async def parse(cls, message) -> tuple[Optional[str], dict]:
+
+		code = message.text.split()
+
+		if not set(code) & cls.__all__:
+			if bot_settings.IS_AI_LOCK:
+				return ChatEnum.AI_LOCK_MSG.value , {"reply_to_message_id": message.message_id}
+			return await AIManager.new_request(message)
+
+		# return await cls._parse_code(code, message)
+
+LeonDSL.__all__ = set(LeonDSL.__members__.keys())
 
 class ChatEnum(Enum):
+	AI_LOCK_MSG = ""
 
 	@classmethod
 	def get_not_authenticate_msg(cls, id, user) -> str:
