78937592d0fd4a422f7bd8386fb2ef272c7df959
GrbigO <gorazbozorg@gmail.com>
2025-06-04 17:27:59 +0330

    Change 'Description Model' to abstract model and Add more abstract model as 'ModelWithDateTime'

--- leonbot/core/models.py
+++ leonbot/core/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 
-class Description(models.Model):
+class ModelWithDescriptions(models.Model):
 	description0 = models.TextField(blank=True)
 	description1 = models.TextField(blank=True)
 	description2 = models.TextField(blank=True)
 	description3 = models.TextField(blank=True)
 
+	class Meta:
+		abstract = True
+
+class ModelWithDateTime(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
