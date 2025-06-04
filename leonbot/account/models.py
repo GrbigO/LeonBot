0555015b1b823a9509df552587a93e9a65b56f31
GrbigO <gorazbozorg@gmail.com>
2025-06-04 17:19:33 +0330

    Add new field phone number for UserTable

--- leonbot/account/models.py
+++ leonbot/account/models.py
@@ -5,6 +5,5 @@ class User(models.Model):
 	name = models.CharField(max_length=100)
 	la_qab = models.CharField(max_length=100)
 	username = models.CharField(max_length=150 , blank=True)
-
-
+	phone = models.CharField(max_length=15 , blank=True)
 
