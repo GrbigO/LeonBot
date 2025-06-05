59c1171de49d2afb6d7a93f455b5f641b2a2d7c0
GrbigO <gorazbozorg@gmail.com>
2025-06-05 21:42:15 +0330

    Add option for chat to bot in PV

--- leonbot/account/models.py
+++ leonbot/account/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
 
 class User(models.Model):
-
 	name = models.CharField(max_length=100)
-	la_qab = models.CharField(max_length=100)
-	username = models.CharField(max_length=150 , blank=True)
-	phone = models.CharField(max_length=15 , blank=True)
 
+	id = models.SmallIntegerField(max_length=50, db_index=True, null=True)
+	username = models.CharField(max_length=200, blank=True)
+
+	pv_log = ArrayField(base_field=models.JSONField(default=dict), default=list)
+	deep = models.CharField(max_length=1000, blank=True)
