b84ce59d904308d436e1246955b10afb24fbde41
GrbigO <gorazbozorg@gmail.com>
2025-06-23 22:56:41 +0330

    Update Handlers

--- leonbot/ai/handlers.py
+++ leonbot/ai/handlers.py
@@ -1,25 +1,22 @@
 import httpx
+from telegram import Update
+
 
 from django.conf import settings as django_settings
 
-from .models import AI
-from ..account.models import User
-from ..botconfs import bot_settings
-from ..chat.utils import slash_n
-from ..core.handlers import UpdateHandler
-from ..core.request import UserRequest
-from ..core.enums import BOTEnum
 
-from .utils import (
-	get_default_name,
-	update_msg_log,
-	user_not_auth_in_this_group,
-)
-
+from ..bot import models
+from ..core.type import Text
+from .utils import get_name
 
 
 class AIRequestHandler:
 
+	DEFAULT_HEADERS = {
+		"Authorization":f"Bearer {django_settings.API_KEY}",
+		"Content-Type":"application/json"
+	}
+	SUCCESS_CODE = 200
 
 	ROLE = "role"
 	CONTENT = "content"
@@ -28,83 +25,60 @@ class AIRequestHandler:
 	USER_ROLE = "user"
 	AI_ROLE = "assistant"
 
-	DEFAULT_SETTINGS = {"reply_to_message_id": message.message_id}
-
-	DEFAULT_HEADERS = {
-		"Authorization":f"Bearer {django_settings.API_KEY}",
-		"Content-Type":"application/json"
-	}
 
 	@classmethod
-	def set_stores(cls, stores: dict):
-		list_of_stores: list[dict] = []
-		for user_name, user_store in stores.values():
-			content = BOTEnum.IM.value + user_name + BOTEnum.DONT_CALL_ME.value + slash_n() + user_store
-			list_of_stores.append(
-				{
-					cls.ROLE: cls.SYSTEM_ROLE,
-					cls.CONTENT: content
-				}
-			)
-		return list_of_stores
-
-
+	def generate_ai_input(cls, role: str, content: str) -> dict:
+		return {cls.ROLE: role, cls.CONTENT: content}
 
 	@classmethod
-	def generate_msg(cls, request: UserRequest):
+	def generate_msg(
+			cls,
+			instance: models.BOT,
+			user: Update.effective_user,
+			message: str,
+			request_in_group: bool
+	):
+
 		msgs = []
-		instance: AI | User = request.instance
-		user = request.metadata.effective_user
-		name = get_default_name(user=user)
-
 		if instance.deep:
-			msgs.append({cls.ROLE: cls.SYSTEM_ROLE, cls.CONTENT: instance.deep})
+			deep = cls.generate_ai_input(role=cls.SYSTEM_ROLE, content=instance.deep)
+			msgs.append(deep)
 
-		if request.in_group:
-
-			if instance.stores.get(user.id) is None:
-				request.funcs.append(user_not_auth_in_this_group)
-			else:
-				name = instance.stores[user.id][0]
-
-			msgs.extend(cls.set_stores(instance.stores))
+		if request_in_group:
+			group_stores = instance.stores
+			msgs.extend(group_stores.comperes())
 
 
 		if instance.msg_logs:
 			msgs.extend(instance.get_msg_logs())
 
-		text = f"{BOTEnum.IM.value + name + BOTEnum.DONT_CALL_ME.value},\n{request.message}"
+		text = Text.new(get_name(instance, user), message)
+		msgs.append({ROLE: USER_ROLE, CONTENT: text})
 
-		msgs.append({cls.ROLE: cls.USER_ROLE, cls.CONTENT: text})
-
-		return (msgs, request)
+		return (msgs, text)
 
 	@classmethod
-	async def new_request(cls, messages: list[dict], request: UserRequest) -> UserRequest:
+	async def new_request(cls, messages: list[dict], instance: models.BOT) -> str | None:
 
 		data = {
-			"model": request.instance.active_model,
+			"model": instance.ai.active_model,
 			"messages": messages,
 		}
 
 		ai_output = await cls.send_request(
 			data=data,
-			headers=cls.DEFAULT_HEADER,
+			headers=cls.DEFAULT_HEADERS,
 		)
 
-		request.funcs.append(update_msg_log)
-		request.ai_output = ai_output
-
-		return request
+		return ai_output
 
 
-	@staticmethod
-	async def send_request(data: dict, headers: dict):
+	async def send_request(cls, data: dict, headers: dict):
 
 		async with httpx.AsyncClient() as client:
 			response = await client.post(django_settings.API_ADDERS, headers=headers, json=data)
 
-		if response.status_code == 200:
+		if response.status_code == cls.SUCCESS_CODE:
 
 			return response.json()["choices"][0]["message"]["content"]
 
