c311763e4f97b4a9a43e54dda895e8f73179a519
GrbigO <gorazbozorg@gmail.com>
2025-06-23 15:14:19 +0330

    Add options for bot settings

--- leonbot/botconfs.py
+++ leonbot/botconfs.py
@@ -1,19 +1,35 @@
 from django.conf import settings
+from .core.emojis import get_emoji
 
 
-def _bot_settings():
+async def __set_request() -> None:
+	bot_settings.REQ_PER += 1
+
+
+def bot_settings():
+	if getattr(bot_settings, "REGISTER", False):
+		raise ValueError("Bot settings already registered")
+
+	bot_settings.REQ_PER = 0
+	bot_settings.HANDLE_REQ = property(__set_request, lambda x: None)
+
+
 	data = settings.BOT_SETTINGS
-	_bot_settings.IS_AI_LOCK = data["IS_LOCK"]
-	_bot_settings.AI_URL = data["AI_URL"]
-	_bot_settings.AI_IDS = data["AI_IDS"]
-	_bot_settings.AI_ID = data["AI_ID"]
-	_bot_settings.GangBang = data["Gang_Bang"]
-	_bot_settings.ACTIVE_MODEL = data["ACTIVE_MODEL"]
-	_bot_settings.DEFAULT_MODEL = data["DEFAULT_MODEL"]
-	_bot_settings.CANCEL_MSG = data["CANCEL_MSG"]
-	_bot_settings.REQUEST_NAME = data["REQUEST_NAME"]
+	bot_settings.TOKEN_BOT = data["TOKEN_BOT"]
+	bot_settings.BOT_USERNAME = data["USERNAME"]
+	bot_settings.REQUEST_NAME = data["REQUEST_NAME"]
+	bot_settings.GROUP_MAX_LENGTH = data["GROUP_MAX_LENGTH"]
+	bot_settings.START_MSG = property(lambda: data["START_MSG"] + get_emoji())
+
+	bot_settings.Type = lambda : bot_settings.Type.__dict__
+	bot_settings.Type.GROUP = ("supergroup", "group")
+	bot_settings.Type.LINK = ("url", "text_link")
+	bot_settings.Type.PV = "private"
 
 
-	return _bot_settings
+	bot_settings.REGISTER = True
+	# Return self func for reset attrs.
+	# If you call again `bot_settings` reset __all__ attribute.
+	return bot_settings
 
-bot_settings = _bot_settings()
+bot_settings = bot_settings()
