4ddecf882ddb87170023cebcff7fa44b2d28c3c8
GrbigO <gorazbozorg@gmail.com>
2025-06-23 14:52:28 +0330

    Refactor manage.py (remove duc string and commands)

--- manage.py
+++ manage.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 if __name__ == "__main__":
-    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leonbot.settings")
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
-
-
