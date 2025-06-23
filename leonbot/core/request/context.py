25cc1d44187a04acdc818180b16fe8b0140029b7
GrbigO <gorazbozorg@gmail.com>
2025-06-23 14:59:23 +0330

    Add RequestContext

---
+++ leonbot/core/request/context.py
@@ -0,0 +1,49 @@
+from typing import Any
+
+from telegram import Update
+
+from leonbot.core.models import BOT
+
+
+class RequestContext:
+	__slots__ = (
+		"for_bot",
+		"for_ai",
+		"in_pv",
+		"in_group",
+		"instance",
+		"update",
+		"context",
+		"mention_user",
+		"first_use",
+	)
+
+	def __init__(
+			self,
+			is_request_for_bot: bool | None = None,
+			is_request_for_ai: bool | None = None,
+			is_request_in_pv: bool | None = None,
+			is_request_in_group: bool | None = None,
+			first_use: bool = False,
+			instance: BOT | None = None,
+			update: Update | None = None,
+			context: Any | None = None,
+	):
+		self.for_bot: bool | None = is_request_for_bot
+		self.for_ai: bool | None = is_request_for_ai
+		self.in_pv: bool | None = is_request_in_pv
+		self.in_group: bool | None = is_request_in_group
+		self.instance: BOT | None = instance
+		self.update: Update | None = update
+		self.context = context
+
+		self.first_use = first_use
+		self.mention_user: bool = False
+
+	@property
+	def user(self):
+		return self.update.effective_user
+
+	@property
+	def user_msg(self):
+		return self.update.message.text
