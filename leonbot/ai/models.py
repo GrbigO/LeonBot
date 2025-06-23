e4aaf4ae692bec19474501eae62809bdf7af4b63
GrbigO <gorazbozorg@gmail.com>
2025-06-23 22:58:25 +0330

    Add Model as "ai_aisettings"

--- leonbot/ai/models.py
+++ leonbot/ai/models.py
@@ -1,20 +1,33 @@
+from functools import partial
+
 from django.db import models
-from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 
-from ..core.models import ModelWithBasicField
+
+from . import Model
+from ..botconfs import bot_settings
 
 
 
-class AI(ModelWithBasicField):
 
-	id = models.IntegerField(primary_key=True)
+class AISettings(models.Model):
+
+	# Field for -Request to AI- `AIRequestHandler`
+	msg_logs = ArrayField(base_field=models.JSONField(default=dict), default=partial(list, dict))
+	deep = models.CharField(max_length=1000, blank=True)
+	active_model = models.CharField(max_length=80, default=Model.DEEPSEEK_v3_0324, choices=Model.CHOICES)
+
+	# Special field for group
 	stores = models.JSONField(default=dict)
 
-	users = models.ManyToManyField(to=settings.AUTH_USER_MODEL, blank=True, related_name="ai_models")
+	update_at = models.DateTimeField(auto_now=True)
 
-	NAME = "Leon AI"
-	VERSION = "v1"
+	def get_msg_logs(self):
+		len_msg_logs = len(self.msg_logs)
 
-	class Meta:
-		db_table = "ai_model"
+		base_index = len_msg_logs - bot_settings.MAX_MSG_LOGS + 2 \
+			if len_msg_logs > bot_settings.MAX_MSG_LOGS else 0
+
+		self.msg_logs = self.msg_logs[base_index: len_msg_logs]
+
+		return self.msg_logs
