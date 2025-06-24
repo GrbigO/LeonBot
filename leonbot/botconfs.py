cfa79899e97945250016ae75580334b06acb61a5
GrbigO <gorazbozorg@gmail.com>
2025-06-24 20:06:41 +0330

    Refactor bot_settings

--- leonbot/botconfs.py
+++ leonbot/botconfs.py
@@ -1,35 +1,44 @@
+from typing import Callable
+import hashlib
+
 from django.conf import settings
+
+from .bot.models import BOT
 from .core.emojis import get_emoji
 
-
-async def __set_request() -> None:
-	bot_settings.REQ_PER += 1
+REQ_PER = 0
 
 
-def bot_settings():
-	if getattr(bot_settings, "REGISTER", False):
-		raise ValueError("Bot settings already registered")
-
-	bot_settings.REQ_PER = 0
-	bot_settings.HANDLE_REQ = property(__set_request, lambda x: None)
+def __set_request() -> None:
+	global REQ_PER
+	REQ_PER += 1
 
 
-	data = settings.BOT_SETTINGS
-	bot_settings.TOKEN_BOT = data["TOKEN_BOT"]
-	bot_settings.BOT_USERNAME = data["USERNAME"]
-	bot_settings.REQUEST_NAME = data["REQUEST_NAME"]
-	bot_settings.GROUP_MAX_LENGTH = data["GROUP_MAX_LENGTH"]
-	bot_settings.START_MSG = property(lambda: data["START_MSG"] + get_emoji())
-
-	bot_settings.Type = lambda : bot_settings.Type.__dict__
-	bot_settings.Type.GROUP = ("supergroup", "group")
-	bot_settings.Type.LINK = ("url", "text_link")
-	bot_settings.Type.PV = "private"
+def __callback_caller(callback: Callable[..., ...]):
+	return callback()
 
 
-	bot_settings.REGISTER = True
+async def bot_settings(init: bool = False, **kwargs):
+
+	if init is True:
+		bot_settings.HANDLE_REQ = __set_request
+		bot_settings.CHOICE_CALL_BOT = (1, 2)
+		bot_settings.CALL = __callback_caller
+
+
+		bot_settings.BOT_USERNAME = settings.BOT_SETTINGS["USERNAME"]
+		bot_settings.REQUEST_NAME = settings.BOT_SETTINGS["REQUEST_NAME"]
+		bot_settings.GROUP_MAX_LENGTH = settings.BOT_SETTINGS["GROUP_MAX_LENGTH"]
+		bot_settings.START_MSG = lambda: settings.BOT_SETTINGS["START_MSG"] + get_emoji()
+
+		bot_settings.Type = {
+			"GROUP": ("supergroup", "group"),
+			"PV": "private",
+			"LINK": ("url", "text_link"),
+		}
+
 	# Return self func for reset attrs.
 	# If you call again `bot_settings` reset __all__ attribute.
+	# this option for update settings and plugins.
 	return bot_settings
 
-bot_settings = bot_settings()
