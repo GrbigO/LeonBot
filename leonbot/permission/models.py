a0d52249ab805104bc913cabfa4c43d0efbc0846
GrbigO <gorazbozorg@gmail.com>
2025-06-24 20:00:09 +0330

    Complete Permission Model and PermissionsMixin

---
+++ leonbot/permission/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Permission(models.Model):
+    codename = models.CharField(max_length=100)
+
+
+class PermissionsMixin(models.Model):
+
+    bot_access = models.ManyToManyField(
+        Permission,
+        blank=True,
+        related_name="bot_set",
+        related_query_name="bot",
+    )
+
+    class Meta:
+        abstract = True
+
