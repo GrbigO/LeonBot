9e2de96612acd43e4f694155d7680a083855d45e
GrbigO <gorazbozorg@gmail.com>
2025-06-29 20:19:44 +0330

    Remove DateField

--- leonbot/bot/models.py
+++ leonbot/bot/models.py
@@ -2,23 +2,16 @@ from django.db import models
 from django.utils import timezone
 
 from ..permission.models import PermissionsMixin
-from ..core.models import ModelWithAuthField
 from ..plugin3.abstracts import ModelWithPluginInstaller
-
-
+from ..core.models import ModelWithAuthField
 
 
 class BOT(ModelWithPluginInstaller, PermissionsMixin):
 	# user_id or group_id or token_bot in telegram
 	telegram_id = models.CharField(max_length=45, primary_key=True, serialize=False)
+
 	ai = models.OneToOneField(to="ai.AISettings", on_delete=models.CASCADE)
 	settings = models.JSONField(default=dict)
 
 	prent = models.ForeignKey(to="self", on_delete=models.CASCADE, null=True)
-
-	last_used = models.DateField(default=timezone.now)
-
-
-	def add_new_log(self, log: dict) -> None:
-		self.ai.msg_logs.append(log)
 
