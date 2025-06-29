5be0a5bb3b325f235043280a0eb4d99c51ccac94
GrbigO <gorazbozorg@gmail.com>
2025-06-29 20:32:18 +0330

    Update settings

--- leonbot/settings.py
+++ leonbot/settings.py
@@ -1,6 +1,5 @@
 import os
-from typing import Any
-
+from typing import Any, Callable
 
 import dj_database_url
 from django.utils.module_loading import import_string
@@ -30,8 +29,15 @@ def get_env(key: str, default: Any = empty) -> str | Any:
 	return value
 
 
-def to_list(text: str):
-	return [name.strip() for name in text.split(",") if name]
+def to_list(text: str, type_convertion: Callable[..., ...] | empty = empty) -> list:
+	if not text: # maybe text == ""
+		return text
+
+	if type_convertion is empty:
+		return [name.strip() for name in text.split(",") if name]
+
+	return [type_convertion(name.strip()) for name in text.split(",") if name]
+
 
 
 # api
@@ -47,36 +53,13 @@ API_KEY = get_env("API_KEY", None)
 WEBHOOK_KEY = get_env("WEBHOOK_KEY", None)
 OPENROUTER_PROVISION_KEY = get_env("OPENROUTER_PROVISION_KEY")
 
-
 # learn how to ues rd.
-BOT_SETTINGS = {
-	"NAME": "Leon",
-	"TOKEN_BOT": get_env("TOKEN_BOT", None),
-	"REQUEST_NAME": get_env("REQUEST_NAME").split(","),
+TOKEN_BOT = get_env("TOKEN_BOT", None)
+BOT_LEADER_IDS = [
 
-	"USERNAME": "@LE0NAI_bot",
-	"GROUP_MAX_LENGTH": 8,
-	"MAX_MSG_LOGS": int(get_env("MAX_MSG_LOGS", 200)),
-
-	"START_MSG": "  If you don't know what this bot is for,\n"
-				 "  what it does,\n"
-				 "  or how to use it,\n"
-				 "  select the -Help- option.\n"
-}
-
-
-INSTALLED_APPS = [
-	# Django modules
-	"django.contrib.contenttypes",
-	"django.contrib.postgres",
-	# Local apps
-	"leonbot.bot",
-	"leonbot.ai",
-	"leonbot.permission",
-	"leonbot.plugin3",
 ]
 
-MIDDLEWARE = []
+
 AUTH_USER_MODEL = "account.User"
 ROOT_URLCONF = ""
 WSGI_APPLICATION = ""
@@ -95,7 +78,32 @@ USE_TZ = True
 STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
+
+INSTALLED_APPS = [
+	# Django modules
+	"django.contrib.postgres",
+	# Local apps
+	"leonbot.core",
+	"leonbot.bot",
+	"leonbot.ai",
+	"leonbot.permission",
+	"leonbot.plugin3",
+]
+
+MIDDLEWARE = []
+
+
 # if GROUP_ID is None:
 
 
 # if API_KEY is None:
+
+
+BOT = {
+	"NAME": "Leon",
+	"REQUEST_NAME": get_env("REQUEST_NAME").split(","),
+
+	"USERNAME": "@LE0NAI_bot",
+	"GROUP_MAX_LENGTH": 8,
+	"MAX_MSG_LOGS": int(get_env("MAX_MSG_LOGS", 200)),
+}
