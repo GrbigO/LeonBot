0398e036426f0c97ff44444a745a1ee5e1f1bcf6
GrbigO <gorazbozorg@gmail.com>
2025-06-29 20:11:42 +0330

    Update handler as 'AIRequestHandler'

--- leonbot/ai/handlers.py
+++ leonbot/ai/handlers.py
@@ -1,12 +1,14 @@
+from typing import Union
+
 import httpx
 from telegram import Update
 
 
 from django.conf import settings as django_settings
 
-
+from ..bot.enums import BOTBrain
+from ..botconfs import bot_settings
 from ..bot import models
-from ..core.type import Text
 from .utils import get_name
 
 
@@ -27,7 +29,7 @@ class AIRequestHandler:
 
 
 	@classmethod
-	def generate_ai_input(cls, role: str, content: str) -> dict:
+	def new_ai_input(cls, role: str, content: str) -> dict:
 		return {cls.ROLE: role, cls.CONTENT: content}
 
 	@classmethod
@@ -37,32 +39,36 @@ class AIRequestHandler:
 			user: Update.effective_user,
 			message: str,
 			request_in_group: bool
-	):
+	) -> tuple[list[dict], str]:
 
-		msgs = []
-		if instance.deep:
-			deep = cls.generate_ai_input(role=cls.SYSTEM_ROLE, content=instance.deep)
+		msgs: list[dict] = []
+
+		msgs.append(cls.new_ai_input(role=cls.SYSTEM_ROLE, content=BOTBrain.get_base_brain()))
+
+		if instance.ai.deep:
+			deep = cls.new_ai_input(role=cls.SYSTEM_ROLE, content=instance.deep)
 			msgs.append(deep)
 
 		if request_in_group:
-			group_stores = instance.stores
-			msgs.extend(group_stores.comperes())
+			for store in instance.ai.generate_stores():
+				msgs.append(cls.new_ai_input(role=cls.SYSTEM_ROLE, content=store))
 
 
-		if instance.msg_logs:
-			msgs.extend(instance.get_msg_logs())
+		if instance.ai.msg_logs:
+			msgs.extend(instance.ai.get_msg_logs())
 
-		text = Text.new(get_name(instance, user), message)
-		msgs.append({ROLE: USER_ROLE, CONTENT: text})
+		text = get_name(instance, user) + " : " + message
+		msgs.append(cls.new_ai_input(role=cls.USER_ROLE, content=text))
 
 		return (msgs, text)
 
 	@classmethod
-	async def new_request(cls, messages: list[dict], instance: models.BOT) -> str | None:
+	async def new_request(cls, messages: list[dict], instance: Union[models.BOT, bot_settings]) -> str | None:
 
 		data = {
-			"model": instance.ai.active_model,
+			"model": "deepseek/deepseek-chat-v3-0324:free",
 			"messages": messages,
+			"max_tokens": 50,
 		}
 
 		ai_output = await cls.send_request(
@@ -72,10 +78,9 @@ class AIRequestHandler:
 
 		return ai_output
 
-
+	@classmethod
 	async def send_request(cls, data: dict, headers: dict):
-
-		async with httpx.AsyncClient() as client:
+		async with httpx.AsyncClient(timeout=30.0) as client:
 			response = await client.post(django_settings.API_ADDERS, headers=headers, json=data)
 
 		if response.status_code == cls.SUCCESS_CODE:
