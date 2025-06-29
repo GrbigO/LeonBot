d3bdadff75b8b6ea343cfe88a10e1d022caba0ed
GrbigO <gorazbozorg@gmail.com>
2025-06-29 20:29:05 +0330

    Update __main__

--- leonbot/__main__.py
+++ leonbot/__main__.py
@@ -1,13 +1,17 @@
-# no webhook just for test
+# No webhook just for test
+# be careful. for use this option your PC or Any
 
 
 if __name__ == "__main__":
+
+	import asyncio
+	import time
+	import sys
 	import os
 	import django
 
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leonbot.settings")
-	django.setup()
-
+	from django.conf import settings as django_settings
+	from telegram.request import HTTPXRequest
 	from telegram.ext import (
 		ApplicationBuilder,
 		CommandHandler,
@@ -15,21 +19,34 @@ if __name__ == "__main__":
 		CallbackQueryHandler,
 		filters,
 	)
-	from .botconfs import bot_settings
-	from leonbot.bot.handlers import ButtonHandler
-	from .chat.manage import ChatHandler
 
-	# run bot
+	# First: Run Django
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leonbot.settings")
+	django.setup()
+
+	from .bot.queryhandlers import QueryHandler
+	from .bot.handlers import ButtonHandler
+	from .chat.manage import ChatHandler
+	from .botconfs import bot_settings
+
+	bot_settings(reload=True)
+
 	print("build...")
-	app = ApplicationBuilder().token(bot_settings.TOKEN_BOT).build()
+	app = ApplicationBuilder().token(django_settings.TOKEN_BOT).build()
 	print("build...done")
 	print("set handler...\n")
 
 	app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), ChatHandler.new_chat))
 	app.add_handler(CommandHandler("start", ButtonHandler.start))
 	app.add_handler(CommandHandler("settings", ButtonHandler.settings))
-	app.add_handler(CallbackQueryHandler(ButtonHandler.button_handler))
 
 	print("leon-bot is rdy for request...")
 
+	if django_settings.DEBUG:
+		def dev_mod_handler(_, context):
+			print(context.error)
+			sys.exit(1)
+
+		app.add_error_handler(dev_mod_handler)
 	app.run_polling()
+
