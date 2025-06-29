4bb36b09eb7cda41bb7a0950221c8fe12e952fea
GrbigO <gorazbozorg@gmail.com>
2025-06-29 20:14:27 +0330

    Add option MSG LOG

--- leonbot/ai/update.py
+++ leonbot/ai/update.py
@@ -2,29 +2,25 @@ from telegram import Update
 
 
 from .handlers import AIRequestHandler
-from ..bot.models import BOT
-from ..core.type import Text
-
+from .models import AISettings
+from ..core.utils import comperes_string
 
 
 async def update_msg_log(
-		instance: BOT,
+		instance: AISettings,
 		new_log: str,
-		ai_output: str | None
+		ai_output: str | None,
 ) -> None:
 
 	ai = AIRequestHandler
-	comperes_new_log = await Text.comperes_string(new_log)
-	new_log = ai.generate_ai_input(role=ai.USER_ROLE, content=comperes_new_log)
-	instance.add_new_log(new_log)
+	new_log = ai.new_ai_input(role=ai.USER_ROLE, content=new_log)
+	instance.new_log(new_log)
 
-	if ai_output is not None:
-		comperes_ai_output = Text.comperes_string(ai_output)
-		ai_output = ai.generate_ai_input(role=ai.AI_ROLE, content=comperes_ai_output)
-		instance.add_new_log(ai_output)
+	if ai_output:
+		ai_output = ai.new_ai_input(role=ai.AI_ROLE, content=ai_output)
+		instance.new_log(ai_output)
 
 
 	await instance.asave(update_fields=["msg_logs"])
 
 	return
-
