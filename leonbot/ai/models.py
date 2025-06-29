10a679e6381b1f5eeceae2dfd13261ace8e8f90c
GrbigO <gorazbozorg@gmail.com>
2025-06-29 20:12:52 +0330

    Change CharField to TextField for active_model Field

--- leonbot/ai/models.py
+++ leonbot/ai/models.py
@@ -13,14 +13,18 @@ from ..botconfs import bot_settings
 class AISettings(models.Model):
 
 	# Field for -Request to AI- `AIRequestHandler`
-	msg_logs = ArrayField(base_field=models.JSONField(default=dict), default=partial(list, dict))
+	msg_logs = ArrayField(base_field=models.JSONField(default=dict), default=partial(list, dict()))
 	deep = models.CharField(max_length=1000, blank=True)
-	active_model = models.CharField(max_length=80, default=Model.DEEPSEEK_v3_0324, choices=Model.CHOICES)
+	active_model = models.TextField(default=Model.DEEPSEEK_v3_0324, choices=Model.CHOICES)
 
 	# Special field for group
 	stores = models.JSONField(default=dict)
 
 	update_at = models.DateTimeField(auto_now=True)
+
+	def new_log(self, log: dict) -> None:
+		self.msg_logs.append(log)
+
 
 	def get_msg_logs(self):
 		len_msg_logs = len(self.msg_logs)
@@ -31,3 +35,8 @@ class AISettings(models.Model):
 		self.msg_logs = self.msg_logs[base_index: len_msg_logs]
 
 		return self.msg_logs
+
+	def generate_stores(self):
+		for _, name, store in self.stores.values():
+			yield name + " : " + store
+
